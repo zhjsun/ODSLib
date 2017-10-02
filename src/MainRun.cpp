052d8ec037424280c4a800e6ef3a46da888aac97
zhjsun <sunzhj01@outlook.com>
2017-10-02 21:36:16 +0200

    Change the type of time in CRightFun

--- src/MainRun.cpp
+++ src/MainRun.cpp
@@ -12,15 +12,17 @@ int main( void ) {
     // Test coordinates transformation
     VectorXd tar(6), cha(6), rel(6);
     ArrayXd Elem(6);
-    tar << 6778137, 1000, 2000, 1, 7000, 1;
+    tar << 6778137, 1000, 000, 1, 7000, 0;
     cout << "Target state:" << endl << tar << endl;
 
     Cart2Elem(tar, Elem);
     cout << "Target element:" << endl << Elem << endl;
 
+    Elem << 6778137, 0.999, 0.0, atan(1.0)*2, atan(1.0)*2, atan(1.0)*2;
     Elem2Cart(Elem, tar);
     cout << "Target state:" << endl << tar << endl;
 
+    /*
     rel << -100, -1000, 50, 1, 1, 1;
     cout << "Chaser in LVLH: " << endl << rel << endl;
 
@@ -35,6 +37,7 @@ int main( void ) {
 
     ICS2LVLH(tar, cha, rel);
     cout << "Chaser in LVLH: " << endl << rel << endl;
+    */
 
     return 0;
 }
